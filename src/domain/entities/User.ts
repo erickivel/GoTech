2e279b4eb592f9496ebad2f4e2d33a6d6566dbb8
Eric Kivel <ericvkivel1@gmail.com>
2022-03-09 23:47:34 -0300

    test: user entity

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,17 +1,46 @@
-import { Entity } from "../../core/domain/Entity";
+import { Either, left, right } from "../../core/logic/Either";
+import { InvalidNameError } from "./errors/InvalidName";
 
 type UserProps = {
+  id: string;
   name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
 };
 
-export class User extends Entity<UserProps> {
+export class User {
+  public readonly name: string;
+  public readonly id: string;
+  public readonly email: string;
+  public readonly password: string;
+  public readonly createdAt: Date;
+
   private constructor(props: UserProps, id?: string) {
-    super(props, id);
+    this.name = props.name;
+    this.id = props.id;
+    this.email = props.email;
+    this.password = props.password;
+    this.createdAt = props.createdAt;
   }
 
-  static create(props: UserProps, id?: string) {
+  static create(props: UserProps, id?: string): Either<InvalidNameError, User> {
+    const validate = User.validate(props.name);
+
+    if (validate.isLeft()) {
+      return left(validate.value);
+    }
+
     const user = new User(props, id);
 
-    return user;
+    return right(user);
+  }
+
+  public static validate(name: string): Either<InvalidNameError, true> {
+    if (name.length > 6) {
+      return left(new InvalidNameError(name));
+    }
+
+    return right(true);
   }
 }
---
+++ src/domain/entities/errors/DomainErrors.ts
@@ -0,0 +1,3 @@
+interface IDomainError {
+  message: string;
+}
