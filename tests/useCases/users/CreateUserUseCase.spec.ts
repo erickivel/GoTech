67ea1fefd285f656995f8b4b450132fdfefbc928
Eric Kivel <ericvkivel1@gmail.com>
2022-03-12 16:46:43 -0300

    test: when invalid params are provided

--- tests/useCases/users/CreateUserUseCase.spec.ts
+++ tests/useCases/users/CreateUserUseCase.spec.ts
@@ -1,8 +1,12 @@
+import { InvalidEmailError } from "../../../src/domain/entities/errors/InvalidEmailError";
+import { InvalidNameError } from "../../../src/domain/entities/errors/InvalidNameError";
+import { InvalidPasswordError } from "../../../src/domain/entities/errors/InvalidPasswordError";
 import { CreateUserUseCase } from "../../../src/useCases/users/CreateUserUseCase";
+import { UserAlreadyExistsError } from "../../../src/useCases/users/errors/UserAlreadyExistsError";
 import { FakeEncoder } from "../../doubles/FakeEncoder";
 import { UsersRepositoryInMemory } from "../../doubles/repositories/UsersRepositoryInMemory";
 
-describe("CreateUser UseCase", () => {
+describe("Create User UseCase", () => {
   let usersRepositoryInMemory: UsersRepositoryInMemory;
   let fakeEncoder: FakeEncoder;
   let createUserUseCase: CreateUserUseCase;
@@ -27,5 +31,59 @@ describe("CreateUser UseCase", () => {
     expect(userOrError.isRight()).toBe(true);
     expect(createdUser).toHaveProperty("id");
     expect(createdUser?.password).not.toBe(user.password);
+  });
+
+  it("should not be able to create a new user if the user already exists", async () => {
+    const user = {
+      name: "John",
+      email: "john@example.com",
+      password: "password",
+    };
+
+    await createUserUseCase.execute(user);
+
+    const theSameUser = await createUserUseCase.execute(user);
+
+    expect(theSameUser.isLeft()).toBe(true);
+    expect(theSameUser.value).toEqual(new UserAlreadyExistsError(user.email));
+  });
+
+  it("should not be able to create a new user if invalid email is provided", async () => {
+    const user = {
+      name: "John",
+      email: "invalid-email.com",
+      password: "password",
+    };
+
+    const userOrError = await createUserUseCase.execute(user);
+
+    expect(userOrError.isLeft()).toBe(true);
+    expect(userOrError.value).toEqual(new InvalidEmailError(user.email));
+  });
+
+  it("should not be able to create a new user if invalid name is provided", async () => {
+    const user = {
+      name: "",
+      email: "john@example.com",
+      password: "password",
+    };
+
+    const userOrError = await createUserUseCase.execute(user);
+
+    expect(userOrError.isLeft()).toBe(true);
+    expect(userOrError.value).toEqual(new InvalidNameError(user.name));
+  });
+
+  it("should not be able to create a new user if invalid password is provided", async () => {
+    const user = {
+      name: "John",
+      email: "john@example.com",
+      password: "short",
+    };
+
+    const userOrError = await createUserUseCase.execute(user);
+
+    expect(userOrError.isLeft()).toBe(true);
+    expect(userOrError.value).toEqual(new InvalidPasswordError());
   });
 });
