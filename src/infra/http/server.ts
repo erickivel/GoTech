fe4a92bfd05f523a2ed097ced44c7b4280953f36
Eric Kivel <ericvkivel1@gmail.com>
2022-03-13 21:18:46 -0300

    refactor: split app

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,16 +1,4 @@
-import "reflect-metadata";
-
-import "../../container";
-
-import express from "express";
-
-import { router } from './routes';
-
-const app = express();
-
-app.use(express.json());
-
-app.use(router);
+import { app } from "./app";
 
 app.listen(3333, () => {
   console.log("🚀 App is running on port 3333!")
