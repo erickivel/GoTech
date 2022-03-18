0e7c277dbacbf8fde8660f431e217ea9a074acae
Eric Kivel <ericvkivel1@gmail.com>
2022-03-17 23:05:24 -0300

    feat: userProfileUseCase

---
+++ src/useCases/users/UserProfileUseCase.ts
@@ -0,0 +1,39 @@
+import { inject, injectable } from "tsyringe";
+
+import { Either, left, right } from "../../core/logic/Either";
+import { IUsersRepository } from "../ports/IUsersRepository";
+import { UserNotFoundError } from "./errors/UserNotFoundError";
+
+interface IRequest {
+  user_id: string;
+};
+
+interface IResponse {
+  id: string;
+  name: string;
+  email: string;
+}
+
+@injectable()
+export class UserProfileUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) { }
+
+  async execute({ user_id }: IRequest): Promise<Either<UserNotFoundError, IResponse>> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      return left(new UserNotFoundError(user_id));
+    };
+
+    const userMapped: IResponse = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    return right(userMapped);
+  };
+};
