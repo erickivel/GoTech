38f33dcc3fbd6c859e2d31373ff36ab1ffc69754
Eric Kivel <ericvkivel1@gmail.com>
2022-03-18 14:48:22 -0300

    feat: userProfileRoute

--- src/infra/http/routes/users.routes.ts
+++ src/infra/http/routes/users.routes.ts
@@ -2,12 +2,18 @@ import { Router } from 'express';
 
 import { CreateUserController } from '../../../controllers/users/CreateUserController';
 import { SignInUserController } from '../../../controllers/users/SignInUserController';
+import { UserProfileController } from '../../../controllers/users/UserProfileController';
+import { adaptedEnsureAuthenticated } from '../middlewares/adaptedEnsureAuthenticated';
 import { routeAdapter } from './RouteAdapter';
 
 export const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
 const signInUserController = new SignInUserController();
+const userProfileController = new UserProfileController();
 
-usersRoutes.post("/cadastrar", routeAdapter(createUserController));
-usersRoutes.post("/entrar", routeAdapter(signInUserController));
+
+
+usersRoutes.post("/signup", routeAdapter(createUserController));
+usersRoutes.post("/signin", routeAdapter(signInUserController));
+usersRoutes.post("/profile", adaptedEnsureAuthenticated, routeAdapter(userProfileController));
