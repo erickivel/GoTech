86cbce4ccb696d186d48b10bd688c7d8c5aee9f2
Eric Kivel <ericvkivel1@gmail.com>
2022-03-21 23:58:33 -0300

    test: listAllUsetsController

---
+++ tests/controllers/users/ListAllUsersController.spec.ts
@@ -0,0 +1,118 @@
+import { container } from "tsyringe";
+
+import { ListAllUsersController } from "../../../src/controllers/users/ListAllUsersController";
+import { JwtAuthenticationTokenProvider } from "../../../src/infra/authentication/JwtAuthenticationTokenProvider";
+import { BcryptEncoder } from "../../../src/infra/encoder/BcryptEncoder";
+import { IAuthenticationTokenProvider } from "../../../src/useCases/users/ports/IAuthenticationTokenProvider";
+import { IEncoder } from "../../../src/useCases/users/ports/IEncoder";
+import { IUsersRepository } from "../../../src/useCases/users/ports/IUsersRepository";
+import { UsersRepositoryInMemory } from "../../doubles/repositories/UsersRepositoryInMemory";
+import { UsersActions } from "../../doubles/UsersActions";
+
+describe("List All Users Controller", () => {
+  let listAllUsersController: ListAllUsersController;
+
+  beforeEach(() => {
+
+    container.registerSingleton<IUsersRepository>("UsersRepository", UsersRepositoryInMemory);
+    container.registerSingleton<IEncoder>("Encoder", BcryptEncoder);
+    container.registerSingleton<IAuthenticationTokenProvider>("AuthenticationTokenProvider", JwtAuthenticationTokenProvider);
+    listAllUsersController = new ListAllUsersController();
+  });
+
+  afterAll(() => {
+    container.clearInstances();
+  });
+
+
+  it("should return status code 200 and user data if user is authenticated", async () => {
+    const usersActions = container.resolve(UsersActions);
+
+    const bcryptEncoder = new BcryptEncoder();
+    const hashedPassword = await bcryptEncoder.encode("password");
+
+    const userCreated = await usersActions.create({
+      id: "fake-id",
+      name: "Admin",
+      email: "admin@example.com",
+      password: hashedPassword,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      isAdmin: true,
+    });
+
+    const fakeRequest = {
+      user: {
+        id: userCreated.id,
+      }
+    }
+
+    const result = await listAllUsersController.handle(fakeRequest);
+
+    const expectedResponse = [
+      {
+        id: userCreated.id,
+        name: "Admin",
+        email: "admin@example.com",
+        createdAt: userCreated.createdAt,
+        updatedAt: userCreated.updatedAt
+      }
+    ];
+
+    expect(result.statusCode).toBe(200);
+    expect(result.body).toEqual(expectedResponse);
+  });
+
+  it("should return status code 403 when trying to list users with invalid id", async () => {
+    const usersActions = container.resolve(UsersActions);
+
+    const bcryptEncoder = new BcryptEncoder();
+    const hashedPassword = await bcryptEncoder.encode("password");
+
+    await usersActions.create({
+      id: "fake-id",
+      name: "Admin",
+      email: "admin@example.com",
+      password: hashedPassword,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      isAdmin: true,
+    });
+
+    const fakeRequest = {
+      user: {
+        id: "invalid-id",
+      }
+    }
+
+    const result = await listAllUsersController.handle(fakeRequest);
+
+    expect(result.statusCode).toBe(403);
+    expect(result.body).toEqual(`User with id "invalid-id" not found`);
+  });
+
+  it("should return status code 403 when the id is missing", async () => {
+    const usersActions = container.resolve(UsersActions);
+
+    const bcryptEncoder = new BcryptEncoder();
+    const hashedPassword = await bcryptEncoder.encode("password");
+
+    await usersActions.create({
+      id: "fake-id",
+      name: "Admin",
+      email: "admin@example.com",
+      password: hashedPassword,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      isAdmin: true,
+    });
+
+    const fakeRequest = {
+    }
+
+    const result = await listAllUsersController.handle(fakeRequest);
+
+    expect(result.statusCode).toBe(400);
+    expect(result.body).toEqual(`Missing parameter(s): user id.`);
+  });
+});
---
+++ tests/doubles/UsersActions.ts
@@ -0,0 +1,18 @@
+import { inject, injectable } from "tsyringe";
+
+import { IUserData } from "../../src/useCases/users/ports/IUserData";
+import { IUsersRepository } from "../../src/useCases/users/ports/IUsersRepository";
+
+@injectable()
+export class UsersActions {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+  ) { }
+
+  async create(data: IUserData): Promise<Omit<IUserData, "password" | "isAdmin">> {
+    const user = await this.usersRepository.create(data);
+
+    return user;
+  };
+};
