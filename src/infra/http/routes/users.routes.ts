7f29aa4800173a101ec1e3a507c34d4cc85fe464
Eric Kivel <ericvkivel1@gmail.com>
2022-03-23 22:02:58 -0300

    feat: list all users route

--- src/infra/http/routes/users.routes.ts
+++ src/infra/http/routes/users.routes.ts
@@ -1,19 +1,24 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '../../../controllers/users/CreateUserController';
+import { ListAllUsersController } from '../../../controllers/users/ListAllUsersController';
 import { UpdateUserController } from '../../../controllers/users/UpdateUserController';
 import { UserProfileController } from '../../../controllers/users/UserProfileController';
+import { adaptedEnsureAdmin } from '../middlewares/adaptedEnsureAdmin';
 import { adaptedEnsureAuthenticated } from '../middlewares/adaptedEnsureAuthenticated';
 import { routeAdapter } from './RouteAdapter';
 
 export const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
-const userProfileController = new UserProfileController();
 const updateUserController = new UpdateUserController();
+const userProfileController = new UserProfileController();
+const listAllUsersController = new ListAllUsersController();
+
 
 
 
 usersRoutes.post("/", routeAdapter(createUserController));
-usersRoutes.get("/profile", adaptedEnsureAuthenticated, routeAdapter(userProfileController));
 usersRoutes.put("/update", adaptedEnsureAuthenticated, routeAdapter(updateUserController));
+usersRoutes.get("/", adaptedEnsureAdmin, routeAdapter(listAllUsersController));
+usersRoutes.get("/profile", adaptedEnsureAuthenticated, routeAdapter(userProfileController));
