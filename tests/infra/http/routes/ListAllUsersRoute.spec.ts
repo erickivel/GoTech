eaee1616cf333b9a310c1fbcd97bf2376f5d5ff6
Eric Kivel <ericvkivel1@gmail.com>
2022-03-23 22:03:11 -0300

    test: listAllUsersRoute

---
+++ tests/infra/http/routes/ListAllUsersRoute.spec.ts
@@ -0,0 +1,87 @@
+import request from 'supertest';
+
+import { prismaClient } from '../../../../src/infra/database/prisma/PrismaClient';
+import { BcryptEncoder } from "../../../../src/infra/encoder/BcryptEncoder";
+import { app } from '../../../../src/infra/http/app';
+
+describe("List All Users Profile Route", () => {
+  let hashedPassword: string;
+  let dateNow: Date;
+
+  beforeAll(async () => {
+    const bcryptEncoder = new BcryptEncoder();
+
+    hashedPassword = await bcryptEncoder.encode("password");
+
+    dateNow = new Date();
+
+    console.log(dateNow);
+
+    await prismaClient.$connect();
+    await prismaClient.users.create({
+      data: {
+        id: "fake id",
+        email: "admin@example.com",
+        name: "Admin",
+        password: hashedPassword,
+        isAdmin: true,
+        createdAt: dateNow,
+        updatedAt: dateNow,
+      }
+    });
+  });
+
+  afterAll(async () => {
+    await prismaClient.users.deleteMany();
+    await prismaClient.$disconnect()
+  });
+
+  it("should return status code 200 and body with all users if user is authenticated and is an admin", async () => {
+    const signInResponse = await request(app)
+      .post("/sessions")
+      .send({
+        email: "admin@example.com",
+        password: "password"
+      });
+
+    const { token } = signInResponse.body;
+
+    const response = await request(app)
+      .get("/users")
+      .set({
+        Authorization: `Bearer ${token}`
+      })
+      .expect(200);
+
+    const expectedResponse = [
+      {
+        id: "fake id",
+        email: "admin@example.com",
+        name: "Admin",
+        createdAt: dateNow.toISOString(),
+        updatedAt: dateNow.toISOString(),
+      }
+    ]
+
+    expect(response.body).toEqual(expectedResponse);
+  });
+
+  it("should return status code 401 and body with unauthorized message if token is invalid", async () => {
+    const response = await request(app)
+      .get("/users")
+      .set({
+        Authorization: `Bearer invalid-token`
+      })
+      .expect(401);
+
+    expect(response.body).toEqual("Token is invalid!");
+  });
+
+  it("should return status code 401 and body with unauthorized message if token is missing", async () => {
+    const response = await request(app)
+      .get("/users")
+      .expect(401);
+
+    expect(response.body).toEqual("Token is missing!");
+  });
+});
