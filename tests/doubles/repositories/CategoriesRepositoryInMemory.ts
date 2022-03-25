3ba6d9a3bd8b26c7d71e4e659570bce6a8603600
Eric Kivel <ericvkivel1@gmail.com>
2022-03-25 16:26:28 -0300

    test: createCategoryUseCase

---
+++ tests/doubles/repositories/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,18 @@
+import { ICategoriesRepository } from "../../../src/useCases/categories/ports/ICategoriesRepository";
+import { ICategoryData } from "../../../src/useCases/categories/ports/ICategoryData";
+
+export class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: ICategoryData[] = [];
+
+  async create(data: ICategoryData): Promise<ICategoryData> {
+    this.categories.push(data);
+
+    return data;
+  };
+
+  async findByName(name: string): Promise<ICategoryData | null> {
+    const category = this.categories.find(category => category.name === name);
+
+    return category || null;
+  }
+}
