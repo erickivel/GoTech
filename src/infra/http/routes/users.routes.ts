82030a7ec22bad33a8f2fa523b7a98d5a1dacad9
Eric Kivel <ericvkivel1@gmail.com>
2022-03-25 14:09:12 -0300

    feat: admin update user route

--- src/infra/http/routes/users.routes.ts
+++ src/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { AdminUpdateUserController } from '../../../controllers/users/AdminUpdateUserController';
 import { CreateUserController } from '../../../controllers/users/CreateUserController';
 import { DeleteUserController } from '../../../controllers/users/DeleteUserController';
 import { ListAllUsersController } from '../../../controllers/users/ListAllUsersController';
@@ -16,9 +17,11 @@ const updateUserController = new UpdateUserController();
 const userProfileController = new UserProfileController();
 const listAllUsersController = new ListAllUsersController();
 const deleteUserController = new DeleteUserController();
+const adminUpdateUserController = new AdminUpdateUserController()
 
 usersRoutes.post("/", routeAdapter(createUserController));
 usersRoutes.put("/update", adaptedEnsureAuthenticated, routeAdapter(updateUserController));
 usersRoutes.get("/", adaptedEnsureAdmin, routeAdapter(listAllUsersController));
 usersRoutes.get("/profile", adaptedEnsureAuthenticated, routeAdapter(userProfileController));
 usersRoutes.delete("/:user_id", adaptedEnsureAdmin, routeAdapter(deleteUserController));
+usersRoutes.put("/admin/update", adaptedEnsureAdmin, routeAdapter(adminUpdateUserController));
