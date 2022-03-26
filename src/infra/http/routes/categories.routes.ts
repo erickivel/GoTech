43d4c466441d2de26eebfc25c935cc2dd40cbe8e
Eric Kivel <ericvkivel1@gmail.com>
2022-03-25 22:42:51 -0300

    feat: list all categories route

--- src/infra/http/routes/categories.routes.ts
+++ src/infra/http/routes/categories.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreateCategoryController } from "../../../controllers/categories/CreateCategoryController";
+import { ListAllCategoriesController } from "../../../controllers/categories/ListAllCategoriesController";
 import { adaptedEnsureAdmin } from "../middlewares/adaptedEnsureAdmin";
 import { routeAdapter } from "./RouteAdapter";
 
 export const categoriesRoutes = Router();
 
 const createCategoryController = new CreateCategoryController();
+const listAllCategoriesController = new ListAllCategoriesController()
 
 categoriesRoutes.post("/", adaptedEnsureAdmin, routeAdapter(createCategoryController));
+categoriesRoutes.get("/", routeAdapter(listAllCategoriesController));
