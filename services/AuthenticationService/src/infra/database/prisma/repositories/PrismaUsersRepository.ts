47781d0f4ff1254878ab5291f86b41a4e704eba5
Eric Kivel <ericvkivel1@gmail.com>
2022-04-03 23:25:52 -0300

    fix: users repository interface

--- services/AuthenticationService/src/infra/database/prisma/repositories/PrismaUsersRepository.ts
+++ services/AuthenticationService/src/infra/database/prisma/repositories/PrismaUsersRepository.ts
@@ -3,6 +3,27 @@ import { IUsersRepository } from '../../../../useCases/authentication/ports/IUse
 import { prismaClient } from '../PrismaClient';
 
 export class PrismaUsersRepository implements IUsersRepository {
+  async create(data: IUserData): Promise<Omit<IUserData, "password" | "isAdmin">> {
+    const userCreated = await prismaClient.users.create({
+      data: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        createdAt: data.createdAt,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        createdAt: true,
+        updatedAt: true,
+      }
+    });
+
+    return userCreated;
+  };
+
   async findByEmail(email: string): Promise<IUserData | null> {
     const user = await prismaClient.users.findFirst({
       where: {
