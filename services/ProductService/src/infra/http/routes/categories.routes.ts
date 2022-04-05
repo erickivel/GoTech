c3f8a7f4ec28a6bcaf00474f4dff430f0f4520b4
Eric Kivel <ericvkivel1@gmail.com>
2022-04-04 22:09:22 -0300

    fix: remove unused routes

--- services/ProductService/src/infra/http/routes/categories.routes.ts
+++ services/ProductService/src/infra/http/routes/categories.routes.ts
@@ -1,19 +1,10 @@
 import { Router } from "express";
 
-import { CreateCategoryController } from "../../../controllers/categories/CreateCategoryController";
-import { DeleteCategoryController } from "../../../controllers/categories/DeleteCategoryController";
 import { ListAllCategoriesController } from "../../../controllers/categories/ListAllCategoriesController";
-import { UpdateCategoryController } from "../../../controllers/categories/UpdateCategoryController";
 import { routeAdapter } from "./RouteAdapter";
 
 export const categoriesRoutes = Router();
 
-const createCategoryController = new CreateCategoryController();
 const listAllCategoriesController = new ListAllCategoriesController();
-const updateCategoryController = new UpdateCategoryController();
-const deleteCategoryController = new DeleteCategoryController();
 
-categoriesRoutes.post("/", routeAdapter(createCategoryController));
-categoriesRoutes.put("/update/:id", routeAdapter(updateCategoryController));
-categoriesRoutes.delete("/delete/:category_id", routeAdapter(deleteCategoryController));
 categoriesRoutes.get("/", routeAdapter(listAllCategoriesController));
